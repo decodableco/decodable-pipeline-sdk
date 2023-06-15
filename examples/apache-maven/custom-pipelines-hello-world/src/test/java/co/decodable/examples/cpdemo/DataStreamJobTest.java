5853e5b02dde87a8c90c113743428359db0a97d0
Gunnar Morling <gunnar.morling@googlemail.com>
2023-06-15 14:15:31 +0200

    #11 Simplifying environment configuration in tests
    Co-authored-by: Robert Metzger <89049+rmetzger@users.noreply.github.com>

--- examples/apache-maven/custom-pipelines-hello-world/src/test/java/co/decodable/examples/cpdemo/DataStreamJobTest.java
+++ examples/apache-maven/custom-pipelines-hello-world/src/test/java/co/decodable/examples/cpdemo/DataStreamJobTest.java
@@ -11,7 +11,6 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.time.Duration;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -35,142 +34,106 @@ import org.testcontainers.redpanda.RedpandaContainer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import co.decodable.sdk.Environment;
+import co.decodable.sdk.EnvironmentAccess;
+import co.decodable.sdk.testing.TestEnvironment;
 
 @Testcontainers
 public class DataStreamJobTest {
 
-  @Container
-  public RedpandaContainer broker =
-      new RedpandaContainer("docker.redpanda.com/redpandadata/redpanda:v23.1.2");
+	private static final String PURCHASE_ORDERS = "purchase-orders";
+	private static final String PURCHASE_ORDERS_PROCESSED = "purchase-orders-processed";
 
-  @Test
-  public void shouldUpperCaseCustomerName() throws Exception {
-    // 1. insert a record into the source stream
-    try (var producer = new KafkaProducer<String, String>(producerProperties())) {
-      String key = "19001";
-      String value = "{\n"
-          + "  \"order_id\" : 19001,\n"
-          + "  \"order_date\" : \"2023-06-09 10:18:38\",\n"
-          + "  \"customer_name\" : \"Muriel Auer\",\n"
-          + "  \"price\" : 15.00,\n"
-          + "  \"product_id\" : 108,\n"
-          + "  \"order_status\" : false\n"
-          + "}";
+	@Container
+	public RedpandaContainer broker =
+	new RedpandaContainer("docker.redpanda.com/redpandadata/redpanda:v23.1.2");
 
-      Future<RecordMetadata> sent =
-          producer.send(new ProducerRecord<String, String>("stream-00000000-ec10a844", key, value));
+	@Test
+	public void shouldUpperCaseCustomerName() throws Exception {
+		TestEnvironment testEnvironment =
+				TestEnvironment.builder()
+				.withBootstrapServers(broker.getBootstrapServers())
+				.withStreams(PURCHASE_ORDERS, PURCHASE_ORDERS_PROCESSED)
+				.build();
 
-      // wait for record to be ack-ed
-      sent.get();
-    }
+		EnvironmentAccess.setEnvironment(testEnvironment);
 
-    String purchaseOrderConfig =
-            "{\n"
-                + "    \"properties\": {\n"
-                + "        \"value.format\": \"debezium-json\",\n"
-                + "        \"key.format\": \"json\",\n"
-                + "        \"topic\": \"stream-00000000-ec10a844\",\n"
-                + "        \"scan.startup.mode\": \"earliest-offset\",\n"
-                + "        \"key.fields\": \"\\\"order_id\\\"\",\n"
-                + "        \"sink.transactional-id-prefix\": \"tx-account-00000000-PIPELINE-af78c091-1686579235527\",\n"
-                + "        \"sink.delivery-guarantee\": \"exactly-once\",\n"
-                + "        \"properties.bootstrap.servers\": \""
-                + broker.getBootstrapServers()
-                + "\",\n"
-                + "        \"properties.transaction.timeout.ms\": \"900000\",\n"
-                + "        \"properties.isolation.level\": \"read_committed\",\n"
-                + "        \"properties.compression.type\": \"zstd\",\n"
-                + "        \"properties.enable.idempotence\": \"true\"\n"
-                + "    },\n"
-                + "    \"name\": \"purchase-orders\"\n"
-                + "}";
+		// 1. insert a record into the source stream
+		try (var producer = new KafkaProducer<String, String>(producerProperties())) {
+			String key = "19001";
+			String value = "{\n"
+					+ "  \"order_id\" : 19001,\n"
+					+ "  \"order_date\" : \"2023-06-09 10:18:38\",\n"
+					+ "  \"customer_name\" : \"Muriel Auer\",\n"
+					+ "  \"price\" : 15.00,\n"
+					+ "  \"product_id\" : 108,\n"
+					+ "  \"order_status\" : false\n"
+					+ "}";
 
-    String purchaseOrderProcessedConfig =
-            "{\n"
-                + "    \"properties\": {\n"
-                + "        \"value.format\": \"debezium-json\",\n"
-                + "        \"key.format\": \"json\",\n"
-                + "        \"topic\": \"stream-00000000-a8da2fca\",\n"
-                + "        \"scan.startup.mode\": \"earliest-offset\",\n"
-                + "        \"key.fields\": \"\\\"order_id\\\"\",\n"
-                + "        \"sink.transactional-id-prefix\": \"tx-account-00000000-PIPELINE-af78c091-1686579235527\",\n"
-                + "        \"sink.delivery-guarantee\": \"exactly-once\",\n"
-                + "        \"properties.bootstrap.servers\": \""
-                + broker.getBootstrapServers()
-                + "\",\n"
-                + "        \"properties.transaction.timeout.ms\": \"900000\",\n"
-                + "        \"properties.isolation.level\": \"read_committed\",\n"
-                + "        \"properties.compression.type\": \"zstd\",\n"
-                + "        \"properties.enable.idempotence\": \"true\"\n"
-                + "    },\n"
-                + "    \"name\": \"purchase-orders-processed\"\n"
-                + "}";
+			Future<RecordMetadata> sent =
+					producer.send(new ProducerRecord<>(testEnvironment.topicFor(PURCHASE_ORDERS), key, value));
 
-        Environment.setEnvironmentConfiguration(
-            Map.of(
-                "DECODABLE_STREAM_CONFIG_ec10a844",
-                purchaseOrderConfig,
-                "DECODABLE_STREAM_CONFIG_a8da2fca",
-                purchaseOrderProcessedConfig));
+			// wait for record to be ack-ed
+			sent.get();
+		}
 
-    // 2. Run the stream processing job
-    CompletableFuture<Void> handle =
-        CompletableFuture.runAsync(
-            () -> {
-              try {
-                DataStreamJob.main(new String[0]);
-              } catch (Exception e) {
-                throw new RuntimeException("Job failed", e);
-              }
-            });
 
-    // 3. assert the processed record on the output stream
-    try (var consumer = new KafkaConsumer<String, String>(consumerProperties())) {
-      consumer.subscribe(List.of("stream-00000000-a8da2fca"));
+		// 2. Run the stream processing job
+		CompletableFuture<Void> handle =
+				CompletableFuture.runAsync(
+						() -> {
+							try {
+								DataStreamJob.main(new String[0]);
+							} catch (Exception e) {
+								throw new RuntimeException("Job failed", e);
+							}
+						});
 
-      Awaitility.await()
-          .atMost(Duration.ofSeconds(10L))
-          .until(
-              () -> {
-                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-                for (ConsumerRecord<String, String> record : records) {
-                  ObjectNode purchaseOrder = (ObjectNode) new ObjectMapper().readTree(record.value());
-                  if (purchaseOrder.get("customer_name").asText().equals("MURIEL AUER")) {
-                    return true;
-                  }
-                }
+		// 3. assert the processed record on the output stream
+		try (var consumer = new KafkaConsumer<String, String>(consumerProperties())) {
+			consumer.subscribe(List.of(testEnvironment.topicFor(PURCHASE_ORDERS_PROCESSED)));
 
-                return false;
-              });
-    } catch (ConditionTimeoutException e) {
-      fail("Expected record not received in time");
-    } finally {
-      try {
-        handle.get(0, TimeUnit.SECONDS);
-      } catch (TimeoutException | ExecutionException | InterruptedException e) {
-        handle.cancel(true);
-      }
-    }
-  }
+			Awaitility.await()
+			.atMost(Duration.ofSeconds(10L))
+			.until(
+					() -> {
+						ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+						for (ConsumerRecord<String, String> record : records) {
+							ObjectNode purchaseOrder = (ObjectNode) new ObjectMapper().readTree(record.value());
+							if (purchaseOrder.get("customer_name").asText().equals("MURIEL AUER")) {
+								return true;
+							}
+						}
 
-  private Properties producerProperties() {
-    var props = new Properties();
-    props.put("bootstrap.servers", broker.getBootstrapServers());
-    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    return props;
-  }
+						return false;
+					});
+		} catch (ConditionTimeoutException e) {
+			fail("Expected record not received in time");
+		} finally {
+			try {
+				handle.get(0, TimeUnit.SECONDS);
+			} catch (TimeoutException | ExecutionException | InterruptedException e) {
+				handle.cancel(true);
+			}
+		}
+	}
 
-  private Properties consumerProperties() {
-    var consumerProps = new Properties();
-    consumerProps.put("bootstrap.servers", broker.getBootstrapServers());
-    consumerProps.put(
-        "key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-    consumerProps.put(
-        "value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-    consumerProps.put("auto.offset.reset", "earliest");
-    consumerProps.put("group.id", "my-group");
-    return consumerProps;
-  }
+	private Properties producerProperties() {
+		var props = new Properties();
+		props.put("bootstrap.servers", broker.getBootstrapServers());
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		return props;
+	}
+
+	private Properties consumerProperties() {
+		var consumerProps = new Properties();
+		consumerProps.put("bootstrap.servers", broker.getBootstrapServers());
+		consumerProps.put(
+				"key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		consumerProps.put(
+				"value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		consumerProps.put("auto.offset.reset", "earliest");
+		consumerProps.put("group.id", "my-group");
+		return consumerProps;
+	}
 }
---
+++ sdk/src/main/java/co/decodable/sdk/testing/TestEnvironment.java
@@ -0,0 +1,138 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright Decodable, Inc.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package co.decodable.sdk.testing;
+
+import co.decodable.sdk.EnvironmentAccess.Environment;
+import co.decodable.sdk.util.Unmodifiable;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * An {@link Environment} implementation for testing purposes, allowing to define one or more
+ * Decodable streams which then can be accessed from the job under test.
+ */
+public class TestEnvironment implements Environment {
+
+  public static class Builder {
+
+    private String bootstrapServers;
+    private final Map<String, StreamConfiguration> streams = new HashMap<>();
+
+    public Builder withBootstrapServers(String bootstrapServers) {
+      Objects.requireNonNull(bootstrapServers, "Bootstrap servers must be specified");
+      this.bootstrapServers = bootstrapServers;
+      return this;
+    }
+
+    public Builder withStreams(String firstStream, String... furtherStreams) {
+      Objects.requireNonNull(firstStream, "At least one stream name must be specified");
+      streams.put(firstStream, new StreamConfiguration(firstStream));
+
+      if (furtherStreams != null) {
+        for (String stream : furtherStreams) {
+          streams.put(stream, new StreamConfiguration(stream));
+        }
+      }
+
+      return this;
+    }
+
+    public TestEnvironment build() {
+      return new TestEnvironment(bootstrapServers, streams);
+    }
+  }
+
+  private static final String STREAM_CONFIG_TEMPLATE =
+      "{\n"
+          + "    \"properties\": {\n"
+          + "        \"value.format\": \"debezium-json\",\n"
+          + "        \"key.format\": \"json\",\n"
+          + "        \"topic\": \"%s\",\n"
+          + "        \"scan.startup.mode\": \"earliest-offset\",\n"
+          + "        \"key.fields\": \"\\\"order_id\\\"\",\n"
+          + "        \"sink.transactional-id-prefix\": \"tx-account-00000000-PIPELINE-af78c091-1686579235527\",\n"
+          + "        \"sink.delivery-guarantee\": \"exactly-once\",\n"
+          + "        \"properties.bootstrap.servers\": \"%s\",\n"
+          + "        \"properties.transaction.timeout.ms\": \"900000\",\n"
+          + "        \"properties.isolation.level\": \"read_committed\",\n"
+          + "        \"properties.compression.type\": \"zstd\",\n"
+          + "        \"properties.enable.idempotence\": \"true\"\n"
+          + "    },\n"
+          + "    \"name\": \"%s\"\n"
+          + "}";
+  @Unmodifiable private final Map<String, StreamConfiguration> streams;
+  private final String bootstrapServers;
+
+  public TestEnvironment(String bootstrapServers, Map<String, StreamConfiguration> streams) {
+    this.bootstrapServers = bootstrapServers;
+    this.streams = Collections.unmodifiableMap(streams);
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  @Override
+  public Map<String, String> getEnvironmentConfiguration() {
+    return streams.entrySet().stream()
+        .collect(
+            Collectors.toUnmodifiableMap(
+                e -> "DECODABLE_STREAM_CONFIG_" + e.getValue().id(),
+                e ->
+                    String.format(
+                        STREAM_CONFIG_TEMPLATE,
+                        e.getValue().topic(),
+                        bootstrapServers,
+                        e.getValue().name())));
+  }
+
+  public String topicFor(String streamName) {
+    StreamConfiguration config = streams.get(streamName);
+
+    if (config == null) {
+      throw new IllegalArgumentException("Stream '" + streamName + "' has not been configured");
+    }
+
+    return config.topic();
+  }
+
+  private static class StreamConfiguration {
+
+    private final String name;
+    private final String id;
+    private final String topic;
+
+    public StreamConfiguration(String name) {
+      this.name = name;
+      this.id = getRandomId();
+      this.topic = "stream-00000000-" + id;
+    }
+
+    private static String getRandomId() {
+      int digits = 8;
+      return String.format("%0" + digits + "x", new BigInteger(digits * 4, new SecureRandom()));
+    }
+
+    public String name() {
+      return name;
+    }
+
+    public String id() {
+      return id;
+    }
+
+    public String topic() {
+      return topic;
+    }
+  }
+}
