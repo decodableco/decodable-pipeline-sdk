4e2da9a8da2599e608a3549a3c6e979f2664b5b7
Gunnar Morling <gunnar.morling@googlemail.com>
2023-06-15 14:04:14 +0200

    #11 Indentation fix

--- examples/apache-maven/custom-pipelines-hello-world/src/test/java/co/decodable/examples/cpdemo/DataStreamJobTest.java
+++ examples/apache-maven/custom-pipelines-hello-world/src/test/java/co/decodable/examples/cpdemo/DataStreamJobTest.java
@@ -40,100 +40,100 @@ import co.decodable.sdk.testing.TestEnvironment;
 @Testcontainers
 public class DataStreamJobTest {
 
-	  private static final String PURCHASE_ORDERS = "purchase-orders";
-	  private static final String PURCHASE_ORDERS_PROCESSED = "purchase-orders-processed";
+	private static final String PURCHASE_ORDERS = "purchase-orders";
+	private static final String PURCHASE_ORDERS_PROCESSED = "purchase-orders-processed";
 
-  @Container
-  public RedpandaContainer broker =
-      new RedpandaContainer("docker.redpanda.com/redpandadata/redpanda:v23.1.2");
+	@Container
+	public RedpandaContainer broker =
+	new RedpandaContainer("docker.redpanda.com/redpandadata/redpanda:v23.1.2");
 
-  @Test
-  public void shouldUpperCaseCustomerName() throws Exception {
-	    TestEnvironment testEnvironment =
-	            TestEnvironment.builder()
-	                .withBootstrapServers(broker.getBootstrapServers())
-	                .withStreams(PURCHASE_ORDERS, PURCHASE_ORDERS_PROCESSED)
-	                .build();
+	@Test
+	public void shouldUpperCaseCustomerName() throws Exception {
+		TestEnvironment testEnvironment =
+				TestEnvironment.builder()
+				.withBootstrapServers(broker.getBootstrapServers())
+				.withStreams(PURCHASE_ORDERS, PURCHASE_ORDERS_PROCESSED)
+				.build();
 
-	    EnvironmentAccess.setEnvironment(testEnvironment);
+		EnvironmentAccess.setEnvironment(testEnvironment);
 
-    // 1. insert a record into the source stream
-    try (var producer = new KafkaProducer<String, String>(producerProperties())) {
-      String key = "19001";
-      String value = "{\n"
-          + "  \"order_id\" : 19001,\n"
-          + "  \"order_date\" : \"2023-06-09 10:18:38\",\n"
-          + "  \"customer_name\" : \"Muriel Auer\",\n"
-          + "  \"price\" : 15.00,\n"
-          + "  \"product_id\" : 108,\n"
-          + "  \"order_status\" : false\n"
-          + "}";
+		// 1. insert a record into the source stream
+		try (var producer = new KafkaProducer<String, String>(producerProperties())) {
+			String key = "19001";
+			String value = "{\n"
+					+ "  \"order_id\" : 19001,\n"
+					+ "  \"order_date\" : \"2023-06-09 10:18:38\",\n"
+					+ "  \"customer_name\" : \"Muriel Auer\",\n"
+					+ "  \"price\" : 15.00,\n"
+					+ "  \"product_id\" : 108,\n"
+					+ "  \"order_status\" : false\n"
+					+ "}";
 
-      Future<RecordMetadata> sent =
-          producer.send(new ProducerRecord<>(testEnvironment.topicFor(PURCHASE_ORDERS), key, value));
+			Future<RecordMetadata> sent =
+					producer.send(new ProducerRecord<>(testEnvironment.topicFor(PURCHASE_ORDERS), key, value));
 
-      // wait for record to be ack-ed
-      sent.get();
-    }
+			// wait for record to be ack-ed
+			sent.get();
+		}
 
 
-    // 2. Run the stream processing job
-    CompletableFuture<Void> handle =
-        CompletableFuture.runAsync(
-            () -> {
-              try {
-                DataStreamJob.main(new String[0]);
-              } catch (Exception e) {
-                throw new RuntimeException("Job failed", e);
-              }
-            });
+		// 2. Run the stream processing job
+		CompletableFuture<Void> handle =
+				CompletableFuture.runAsync(
+						() -> {
+							try {
+								DataStreamJob.main(new String[0]);
+							} catch (Exception e) {
+								throw new RuntimeException("Job failed", e);
+							}
+						});
 
-    // 3. assert the processed record on the output stream
-    try (var consumer = new KafkaConsumer<String, String>(consumerProperties())) {
-      consumer.subscribe(List.of(testEnvironment.topicFor(PURCHASE_ORDERS_PROCESSED)));
+		// 3. assert the processed record on the output stream
+		try (var consumer = new KafkaConsumer<String, String>(consumerProperties())) {
+			consumer.subscribe(List.of(testEnvironment.topicFor(PURCHASE_ORDERS_PROCESSED)));
 
-      Awaitility.await()
-          .atMost(Duration.ofSeconds(10L))
-          .until(
-              () -> {
-                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-                for (ConsumerRecord<String, String> record : records) {
-                  ObjectNode purchaseOrder = (ObjectNode) new ObjectMapper().readTree(record.value());
-                  if (purchaseOrder.get("customer_name").asText().equals("MURIEL AUER")) {
-                    return true;
-                  }
-                }
+			Awaitility.await()
+			.atMost(Duration.ofSeconds(10L))
+			.until(
+					() -> {
+						ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+						for (ConsumerRecord<String, String> record : records) {
+							ObjectNode purchaseOrder = (ObjectNode) new ObjectMapper().readTree(record.value());
+							if (purchaseOrder.get("customer_name").asText().equals("MURIEL AUER")) {
+								return true;
+							}
+						}
 
-                return false;
-              });
-    } catch (ConditionTimeoutException e) {
-      fail("Expected record not received in time");
-    } finally {
-      try {
-        handle.get(0, TimeUnit.SECONDS);
-      } catch (TimeoutException | ExecutionException | InterruptedException e) {
-        handle.cancel(true);
-      }
-    }
-  }
+						return false;
+					});
+		} catch (ConditionTimeoutException e) {
+			fail("Expected record not received in time");
+		} finally {
+			try {
+				handle.get(0, TimeUnit.SECONDS);
+			} catch (TimeoutException | ExecutionException | InterruptedException e) {
+				handle.cancel(true);
+			}
+		}
+	}
 
-  private Properties producerProperties() {
-    var props = new Properties();
-    props.put("bootstrap.servers", broker.getBootstrapServers());
-    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    return props;
-  }
+	private Properties producerProperties() {
+		var props = new Properties();
+		props.put("bootstrap.servers", broker.getBootstrapServers());
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		return props;
+	}
 
-  private Properties consumerProperties() {
-    var consumerProps = new Properties();
-    consumerProps.put("bootstrap.servers", broker.getBootstrapServers());
-    consumerProps.put(
-        "key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-    consumerProps.put(
-        "value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-    consumerProps.put("auto.offset.reset", "earliest");
-    consumerProps.put("group.id", "my-group");
-    return consumerProps;
-  }
+	private Properties consumerProperties() {
+		var consumerProps = new Properties();
+		consumerProps.put("bootstrap.servers", broker.getBootstrapServers());
+		consumerProps.put(
+				"key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		consumerProps.put(
+				"value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		consumerProps.put("auto.offset.reset", "earliest");
+		consumerProps.put("group.id", "my-group");
+		return consumerProps;
+	}
 }
