0c9021522e026db8e26379aea97a7bdbdcce6a17
Nicolaus Weidner <nico@decodable.co>
2023-11-21 16:44:45 +0100

    #3 feat: Add JavaDocs for public API classes

--- sdk/src/main/java/co/decodable/sdk/pipeline/DecodableSecret.java
+++ sdk/src/main/java/co/decodable/sdk/pipeline/DecodableSecret.java
@@ -7,21 +7,36 @@
  */
 package co.decodable.sdk.pipeline;
 
+import co.decodable.sdk.pipeline.exception.SecretNotFoundException;
 import co.decodable.sdk.pipeline.internal.DecodableSecretImpl;
 import java.time.Instant;
 
+/**
+ * Represents a <a href="https://docs.decodable.co/docs/manage-secrets">Decodable secret</a>.
+ * Exposes both metadata and the secret value.
+ */
 public interface DecodableSecret {
+  /** Returns the plaintext secret value. */
   String value();
 
+  /** Returns the name of the secret. */
   String name();
 
+  /** Returns the secret description. */
   String description();
 
+  /** Returns the creation time of the secret. */
   Instant createTime();
 
+  /** Returns the time the secret was last updated. */
   Instant updateTime();
 
-  static DecodableSecret withName(String name) {
+  /**
+   * Looks up the secret by name and returns a {@link DecodableSecret} instance.
+   *
+   * @throws SecretNotFoundException if the secret is not present or cannot be read
+   */
+  static DecodableSecret withName(String name) throws SecretNotFoundException {
     return new DecodableSecretImpl(name);
   }
 }
--- sdk/src/main/java/co/decodable/sdk/pipeline/exception/SecretNotFoundException.java
+++ sdk/src/main/java/co/decodable/sdk/pipeline/exception/SecretNotFoundException.java
@@ -7,6 +7,11 @@
  */
 package co.decodable.sdk.pipeline.exception;
 
+/**
+ * This exception is thrown when a <a href="https://docs.decodable.co/docs/manage-secrets">Decodable
+ * secret</a> cannot be accessed. The most probable cause is that it was not referenced in the
+ * pipeline's properties.
+ */
 public class SecretNotFoundException extends RuntimeException {
   public SecretNotFoundException(String message) {
     super(message);
