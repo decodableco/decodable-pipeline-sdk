fccca52fa61b4729bb8e52dd6c815a0f7e297a50
Nicolaus Weidner <nico@decodable.co>
2023-12-18 19:03:36 +0100

    #45 chore: Add warning if neither source nor sink streams are declared

--- sdk/src/test/java/co/decodable/sdk/pipeline/internal/config/metadata/MetadataProcessorTest.java
+++ sdk/src/test/java/co/decodable/sdk/pipeline/internal/config/metadata/MetadataProcessorTest.java
@@ -8,6 +8,7 @@
 package co.decodable.sdk.pipeline.internal.config.metadata;
 
 import static com.google.testing.compile.CompilationSubject.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import co.decodable.sdk.pipeline.internal.metadata.MetadataProcessor;
 import com.google.common.io.CharSource;
@@ -18,10 +19,18 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 import javax.tools.StandardLocation;
 import org.junit.jupiter.api.Test;
 
 public class MetadataProcessorTest {
+
+  private static final String OUTPUT_PATH = "META-INF/decodable/stream-names.properties";
 
   @Test
   public void shouldGenerateStreamNamesFile() throws MalformedURLException {
@@ -38,10 +47,56 @@ public class MetadataProcessorTest {
 
     assertThat(compilation).succeeded();
     assertThat(compilation)
-        .generatedFile(StandardLocation.CLASS_OUTPUT, "META-INF/decodable/stream-names.properties")
+        .generatedFile(StandardLocation.CLASS_OUTPUT, OUTPUT_PATH)
         .hasContents(
             CharSource.wrap(
                     "source-streams=purchase-orders\nsink-streams=purchase-orders-processed\n")
                 .asByteSource(Charset.forName("UTF-8")));
+  }
+
+  @Test
+  public void shouldWarnIfNoStreamsAreDeclared() throws MalformedURLException {
+    var logger = Logger.getLogger(MetadataProcessor.class.getName());
+    var handler = new TestHandler();
+    logger.setUseParentHandlers(false);
+    logger.addHandler(handler);
+    var fileWithoutAnnotations =
+        new File(
+                "./src/test/java/co/decodable/sdk/pipeline/internal/config/metadata/MetadataProcessorTest.java")
+            .toURI()
+            .toURL();
+
+    var compilation =
+        Compiler.javac()
+            .withProcessors(new MetadataProcessor())
+            .compile(JavaFileObjects.forResource(fileWithoutAnnotations));
+
+    assertThat(compilation).succeeded();
+    assertThat(handler.getRecords()).hasSize(1);
+    var record = handler.getRecords().get(0);
+    assertThat(record.getLevel()).isEqualTo(Level.WARNING);
+    assertThat(record.getMessage()).contains("Neither source nor sink streams were declared");
+  }
+
+  private static class TestHandler extends Handler {
+
+    private final List<LogRecord> records = new ArrayList<>();
+
+    @Override
+    public void publish(LogRecord record) {
+      records.add(record);
+    }
+
+    @Override
+    public void flush() {
+      records.clear();
+    }
+
+    @Override
+    public void close() throws SecurityException {}
+
+    public List<LogRecord> getRecords() {
+      return records;
+    }
   }
 }
