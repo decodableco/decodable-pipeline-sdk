2a594b55b8a80a6bf7d2de3e888bea7a53664b7e
Sharon Xie <rxie@decodable.co>
2025-02-14 16:59:18 -0800

    spotless

--- sdk/src/test/java/co/decodable/sdk/pipeline/TableAPIJobTest.java
+++ sdk/src/test/java/co/decodable/sdk/pipeline/TableAPIJobTest.java
@@ -21,64 +21,61 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.redpanda.RedpandaContainer;
 
-// spotless:off
 @Testcontainers // @start region="testing-custom-pipeline"
 public class TableAPIJobTest {
 
-    static final String PURCHASE_ORDERS_STREAM = "purchase-orders";
-    static final String PURCHASE_ORDERS_PROCESSED_STREAM = "purchase-orders-processed";
+  static final String PURCHASE_ORDERS = "purchase-orders";
+  static final String PURCHASE_ORDERS_PROCESSED = "purchase-orders-processed";
 
-    @Container
-    public RedpandaContainer broker =
-            new RedpandaContainer("docker.redpanda.com/redpandadata/redpanda:v23.1.2");
+  @Container
+  public RedpandaContainer broker =
+      new RedpandaContainer("docker.redpanda.com/redpandadata/redpanda:v23.1.2");
 
-    @Test
-    public void runTest() throws Exception {
-        TestEnvironment testEnvironment =
-                TestEnvironment.builder()
-                        .withBootstrapServers(broker.getBootstrapServers())
-                        .withStreams(PURCHASE_ORDERS_STREAM, PURCHASE_ORDERS_PROCESSED_STREAM)
-                        .build();
+  @Test
+  public void runTest() throws Exception {
+    TestEnvironment testEnvironment =
+        TestEnvironment.builder()
+            .withBootstrapServers(broker.getBootstrapServers())
+            .withStreams(PURCHASE_ORDERS, PURCHASE_ORDERS_PROCESSED)
+            .build();
 
-        try (PipelineTestContext ctx = new PipelineTestContext(testEnvironment)) {
-            String value =
-                    "{\n"
-                            + "  \"order_id\" : 19001,\n"
-                            + "  \"order_date\" : \"2023-06-09 10:18:38\",\n"
-                            + "  \"customer_name\" : \"Yolanda Hagenes\",\n"
-                            + "  \"price\" : 15.00,\n"
-                            + "  \"product_id\" : 108,\n"
-                            + "  \"order_status\" : false\n"
-                            + "}";
-            String value2 =
-                    "{\n"
-                            + "  \"order_id\" : 19002,\n"
-                            + "  \"order_date\" : \"2023-06-09 11:25:33\",\n"
-                            + "  \"customer_name\" : \"Erwin Mausepeter\",\n"
-                            + "  \"price\" : 35.00,\n"
-                            + "  \"product_id\" : 22,\n"
-                            + "  \"order_status\" : false\n"
-                            + "}";
+    try (PipelineTestContext ctx = new PipelineTestContext(testEnvironment)) {
+      String value =
+          "{\n"
+              + "  \"order_id\" : 19001,\n"
+              + "  \"order_date\" : \"2023-06-09 10:18:38\",\n"
+              + "  \"customer_name\" : \"Yolanda Hagenes\",\n"
+              + "  \"price\" : 15.00,\n"
+              + "  \"product_id\" : 108,\n"
+              + "  \"order_status\" : false\n"
+              + "}";
+      String value2 =
+          "{\n"
+              + "  \"order_id\" : 19002,\n"
+              + "  \"order_date\" : \"2023-06-09 11:25:33\",\n"
+              + "  \"customer_name\" : \"Erwin Mausepeter\",\n"
+              + "  \"price\" : 35.00,\n"
+              + "  \"product_id\" : 22,\n"
+              + "  \"order_status\" : false\n"
+              + "}";
 
-            // given
-            ctx.stream(PURCHASE_ORDERS_STREAM).add(new StreamRecord<>(value));
-            ctx.stream(PURCHASE_ORDERS_STREAM).add(new StreamRecord<>(value2));
+      // given
+      ctx.stream(PURCHASE_ORDERS).add(new StreamRecord<>(value));
+      ctx.stream(PURCHASE_ORDERS).add(new StreamRecord<>(value2));
 
-            ctx.runJobAsync(PurchasingOrderProcessingTableAPIJob::main);
+      ctx.runJobAsync(PurchasingOrderProcessingTableAPIJob::main);
 
-            StreamRecord<String> result =
-                    ctx.stream(PURCHASE_ORDERS_PROCESSED_STREAM).takeOne().get(30, TimeUnit.SECONDS);
-            StreamRecord<String> result2 =
-                    ctx.stream(PURCHASE_ORDERS_PROCESSED_STREAM).takeOne().get(30, TimeUnit.SECONDS);
-            ObjectNode purchaseOrder = (ObjectNode) new ObjectMapper().readTree(result.value());
-            ObjectNode purchaseOrder2 = (ObjectNode) new ObjectMapper().readTree(result2.value());
+      StreamRecord<String> result =
+          ctx.stream(PURCHASE_ORDERS_PROCESSED).takeOne().get(30, TimeUnit.SECONDS);
+      StreamRecord<String> result2 =
+          ctx.stream(PURCHASE_ORDERS_PROCESSED).takeOne().get(30, TimeUnit.SECONDS);
+      ObjectNode purchaseOrder = (ObjectNode) new ObjectMapper().readTree(result.value());
+      ObjectNode purchaseOrder2 = (ObjectNode) new ObjectMapper().readTree(result2.value());
 
-            // then
-            assertThat(purchaseOrder.get("customer_name").asText()).isEqualTo("YOLANDA HAGENES");
-            assertThat(purchaseOrder2.get("customer_name").asText()).isEqualTo("ERWIN MAUSEPETER");
-        }
-
-
-    } // @end region="custom-pipeline"
+      // then
+      assertThat(purchaseOrder.get("customer_name").asText()).isEqualTo("YOLANDA HAGENES");
+      assertThat(purchaseOrder2.get("customer_name").asText()).isEqualTo("ERWIN MAUSEPETER");
+    }
+  } // @end region="custom-pipeline"
 }
-//spotless:on
+// spotless:on
